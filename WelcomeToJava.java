718542dfa5469a4bb7807c730313007d8757f72b
Suman <42217495+sumanchatterjee3@users.noreply.github.com>
2018-12-04 23:33:04 +0530

    Delete WelcomeToJava.java

--- WelcomeToJava.java
+++
@@ -1,8 +0,0 @@
-package com.suman.HackerRank;
-
-public class WelcomeToJava {
-    public static void main(String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-}
